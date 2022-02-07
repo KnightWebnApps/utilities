b6d9026017a03fa013ada5cd51c31f28344afcc0
KnightWebnApps <45805254+KnightWebnApps@users.noreply.github.com>
2022-02-06 20:16:43 -0500

    Added Closing, Footer, Bullets and Nav components

---
+++ components/src/lib/Bullets.svelte
@@ -0,0 +1,59 @@
+<script>
+	export let copy = {
+		headline: '',
+		subhead: '',
+		bullets: ["Here's a bullet"]
+	};
+</script>
+
+<section id="bullets">
+	<div class="container">
+		{#if copy.headline && copy.headline.length > 0}
+			<h3>{copy.headline}</h3>
+		{/if}
+		{#if copy.subhead && copy.subhead.length > 0}
+			<p>{copy.subhead}</p>
+		{/if}
+		<ul class="bi-column">
+			{#each copy.bullets as bullet}
+				<li>{bullet}</li>
+			{/each}
+		</ul>
+		{#if copy.callToAction}
+			<a href={copy.href} role="button">{copy.callToAction}</a>
+		{/if}
+	</div>
+</section>
+
+<style>
+	#bullets {
+		background-color: var(--primary);
+		width: 100vw;
+		margin: 0 0 0 -50vw;
+		position: relative;
+		left: 50%;
+		top: 0;
+		padding: calc(var(--spacing) * 2);
+		margin-bottom: calc(var(--spacing) * 4);
+	}
+
+	#bullets > .container > * {
+		color: #000;
+	}
+
+	li {
+		font-size: 20px;
+	}
+
+	.bi-column {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: var(--spacing);
+	}
+
+	@media (max-width: 600px) {
+		.bi-column {
+			grid-template-columns: 1fr;
+		}
+	}
+</style>
---
+++ components/src/lib/Footer.svelte
@@ -0,0 +1,22 @@
+<script>
+	// import Facebook from 'virtual:icons/carbon/logo-facebook'
+	const year = new Date().getFullYear();
+</script>
+
+<footer>
+	<p>Copyright &copy; {year} Knight Digital Development, LLC</p>
+	<ul>
+		<!-- <li>
+      <a href="/"> -->
+		<!-- <Facebook /> -->
+		<!-- </a>
+    </li> -->
+	</ul>
+</footer>
+
+<style>
+	a {
+		width: 42px;
+		height: 42px;
+	}
+</style>
